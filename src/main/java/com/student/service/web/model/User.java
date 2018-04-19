1d52c83ca166184b49b9a30f6ce62408586cac21
Elleshar000 <elleshar00@gmail.com>
2018-04-19 15:13:26 +0200

    user model class added

--- src/main/java/com/student/service/web/model/User.java
+++ src/main/java/com/student/service/web/model/User.java
@@ -1,4 +1,131 @@
 package com.student.service.web.model;
 
-public class User {
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private int id;
+
+    @NotBlank
+    @Column
+    private String email;
+
+    @NotBlank
+    @Column
+    private String name;
+
+    @NotBlank
+    @Column
+    private String lastname;
+
+    @NotBlank
+    @Column
+    private String password;
+
+    @NotBlank
+    @Column
+    private String education;
+
+    @NotBlank
+    @Column(name = "school_id")
+    private int schoolId;
+
+    @Column
+    private String department;
+
+    @Column
+    private String specialization;
+
+    @Column(name = "rate_avg")
+    private double rateAvg;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public int getSchoolId() {
+        return schoolId;
+    }
+
+    public void setSchoolId(int schoolId) {
+        this.schoolId = schoolId;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public double getRateAvg() {
+        return rateAvg;
+    }
+
+    public void setRateAvg(double rateAvg) {
+        this.rateAvg = rateAvg;
+    }
+
 }
