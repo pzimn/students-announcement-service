d7f525d48fa512d671bee5da6c20e9008f491b7b
Michal <Michal@DESKTOP-NEHF7MV>
2019-01-28 15:34:14 +0000

    /myAnnouncements added

--- src/main/java/com/student/service/web/controller/AnnouncementController.java
+++ src/main/java/com/student/service/web/controller/AnnouncementController.java
@@ -2,57 +2,76 @@ package com.student.service.web.controller;
 
 import com.student.service.web.model.Announcement;
 import com.student.service.web.service.facade.api.AnnouncementService;
+import com.student.service.web.service.facade.api.UserService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class AnnouncementController {
 
-    private final Logger logger = LoggerFactory.getLogger(getClass().getName());
+	private final Logger logger = LoggerFactory.getLogger(getClass().getName());
 
-    @Autowired
-    AnnouncementService announcementService;
+	@Autowired
+	AnnouncementService announcementService;
 
-    // Get All Announcements
-    @GetMapping("/announcements")
-    public List<Announcement> getAllAnnouncement() {
-        return announcementService.findAll();
-    }
+	@Autowired
+	UserService userService;
 
-    // Create a new Announcement
-    @PostMapping("/announcements")
-    public Announcement createAnnouncement(@RequestBody Announcement announcement) {
-        logger.info("Saving announcement: {}. ", announcement);
-        return announcementService.save(announcement);
-    }
+	// Get All Announcements
+	@GetMapping("/myAnnouncements")
+	public ResponseEntity<?> getAllAnnouncementByUser(Principal principal) {
+		if (principal == null)
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+		int userId = userService.findByUsername(principal.getName()).getId();
+		return new ResponseEntity<List<Announcement>>(announcementService.findAll().stream().filter(user -> user.getUserId() == userId)
+				.collect(Collectors.toList()), HttpStatus.ACCEPTED);
+	}
 
-    // Get a Single Announcement
-    @GetMapping("/announcements/{id}")
-    public List<Announcement> getAnnouncementById(@PathVariable(value = "id") Integer announcementId) {
-        List<Announcement> announcement = announcementService.findAllDownParentCategory(announcementId);
-        logger.info("Retrieved user from DB: {}. ",announcement);
-        return announcement;
-    }
+	@GetMapping("/announcements")
+	public List<Announcement> getAllAnnouncement() {
+		return announcementService.findAll();
+	}
 
-    // Update a Announcement
-    @PutMapping("/announcements")
-    public Announcement updateAnnouncement(@RequestBody Announcement announcement) {
-        logger.info("Updating announcement in DB: {}. ", announcement);
-        announcementService.save(announcement);
-        logger.info("Updated announcement in DB: {}. ", announcement);
-        return announcement;
-    }
+	// Create a new Announcement
+	@PostMapping("/announcements")
+	public Announcement createAnnouncement(@RequestBody Announcement announcement) {
+		logger.info("Saving announcement: {}. ", announcement);
+		return announcementService.save(announcement);
+	}
 
-    // Delete a Announcement
-    @DeleteMapping("/announcements/{id}")
-    public void deleteAnnouncement(@PathVariable("id") Integer announcementId) {
-        logger.info("Deleting announcement from DB with ID: {}. ", announcementId);
-        announcementService.deleteById(announcementId);
-    }
+	// Get a Single Announcement
+	@GetMapping("/announcements/{id}")
+	public List<Announcement> getAnnouncementById(@PathVariable(value = "id") Integer announcementId) {
+		List<Announcement> announcement = announcementService.findAllDownParentCategory(announcementId);
+		logger.info("Retrieved user from DB: {}. ", announcement);
+		return announcement;
+	}
+
+	// Update a Announcement
+	@PutMapping("/announcements")
+	public Announcement updateAnnouncement(@RequestBody Announcement announcement) {
+		logger.info("Updating announcement in DB: {}. ", announcement);
+		announcementService.save(announcement);
+		logger.info("Updated announcement in DB: {}. ", announcement);
+		return announcement;
+	}
+
+	// Delete a Announcement
+	@DeleteMapping("/announcements/{id}")
+	public void deleteAnnouncement(@PathVariable("id") Integer announcementId) {
+		logger.info("Deleting announcement from DB with ID: {}. ", announcementId);
+		announcementService.deleteById(announcementId);
+	}
 
 }
